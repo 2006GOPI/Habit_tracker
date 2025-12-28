35082290f94c9c5eca64fd1bfb81c01438277746
gopi <gopi200616@gmail.com>
2025-12-28 22:23:45 +0530

    Fix: Use Regex for catch-all route

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ app.use('/api/history', historyRoutes);
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('/:path(.*)', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
